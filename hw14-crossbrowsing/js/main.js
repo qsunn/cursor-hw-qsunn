db02b567be6b953223a190c0999982914f4ac2f9
Volodymyr Boychyshyn <qsunnv@gmail.com>
2021-12-22 16:51:36 +0200

    fix #1

--- hw14-crossbrowsing/js/main.js
+++ hw14-crossbrowsing/js/main.js
@@ -60,3 +60,21 @@ window.onload = function () {
         document.body.classList.remove('loaded_hiding');
     }, 500);
 }
+
+
+
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video', {
+        height: '390',
+        width: '640',
+        videoId: 'M7lc1UVf-VE',
+    });
+}
