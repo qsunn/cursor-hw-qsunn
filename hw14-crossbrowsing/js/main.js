1668e93b10efca68228eec56c085968c609751ca
Volodymyr Boychyshyn <qsunnv@gmail.com>
2021-12-22 16:25:47 +0200

    added hw#14

---
+++ hw14-crossbrowsing/js/main.js
@@ -0,0 +1,62 @@
+const prev = document.querySelector('.arrow-left')
+const next = document.querySelector('.arrow-right')
+const slides = document.querySelectorAll('.slide')
+
+let index = 0
+
+const activeSlide = n => {
+    for (const slide of slides) {
+        slide.classList.remove('active')
+    }
+    slides[n].classList.add('active')
+}
+
+const prevSlide = () => {
+    if (index == 0) {
+        index = slides.length - 1
+        activeSlide(index)
+    } else {
+        index--
+        activeSlide(index)
+    }
+}
+
+const nextSlide = () => {
+    if (index == slides.length - 1) {
+        index = 0
+        activeSlide(index)
+    } else {
+        index++
+        activeSlide(index)
+    }
+}
+
+const stopInterval = () => {
+    clearInterval(interval)
+}
+
+prev.addEventListener('click', prevSlide)
+next.addEventListener('click', nextSlide)
+next.addEventListener('click', stopInterval)
+prev.addEventListener('click', stopInterval)
+
+document.addEventListener('keydown',
+event => {
+        if (event.key === 'ArrowRight') {
+        nextSlide()
+        } else if (event.key === 'ArrowLeft') {
+        prevSlide()
+    }
+})
+
+const interval = setInterval(() => {
+    nextSlide()
+}, 2400)
+
+window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+    }, 500);
+}
